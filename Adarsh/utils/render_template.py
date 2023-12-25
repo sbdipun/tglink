a95210a72383283f9e3c38e16309486dcc86f15c
sahasudip2006 <127957723+sahasudip2006@users.noreply.github.com>
2023-12-25 16:44:26 +0530

    Update render_template.py

--- Adarsh/utils/render_template.py
+++ Adarsh/utils/render_template.py
@@ -50,7 +50,7 @@ async def render_page(id, secure_hash):
 </p>
 <center>
     <h2>
-        <a href="https://telegram.dog/+kc6bYRCsWdlhOTI1">
+        <a href="https://t.me/main_channel_bot_update">
             <img src="https://graph.org/file/b57cdba982191a25db535.jpg" alt="Rkbotz" width="150" height="75">
         </a>
     </h2>
